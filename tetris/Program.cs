048cceb8b2950451975793d4267ce3a337f5b982
Alexey Tolkov <TolkovAS@GMail.com>
2021-03-25 11:24:49 +0500

    test1

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -7,6 +7,12 @@ namespace tetris
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hi!");
+
+            Console.WriteLine("Hi2");
+
+            Console.WriteLine("sdofksnedlfknsdlkf");
         }
     }
 }
